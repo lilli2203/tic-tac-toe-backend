cf9460789ad41d3a7221b08c95b3f22220164b0f
lilli2203 <mailyourlilli@gmail.com>
2024-07-11 19:57:56 +0530

    Update GameController.java

--- controllers/GameController.java
+++ controllers/GameController.java
@@ -1,22 +1,30 @@
 package com.example.controllers;
 
 import com.example.exceptions.InvalidMoveException;
-import com.example.models.Game;
-import com.example.models.GameState;
-import com.example.models.Player;
+import com.example.models.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GameController {
-    public Game startGame(int dimension, List<Player> players) {
-        //TODO
-        //Validate if 2 players have the same symbol or not ?
-        //If 2 players have same symbol, throw some exception.
+    public Game startGame(int dimension, List<Player> players) throws InvalidMoveException {
+        validatePlayers(players);
         return new Game(dimension, players);
+    }
+
+    private void validatePlayers(List<Player> players) throws InvalidMoveException {
+        Set<Character> symbols = new HashSet<>();
+        for (Player player : players) {
+            if (!symbols.add(player.getSymbol().getSymbol())) {
+                throw new InvalidMoveException("Two players cannot have the same symbol: " + player.getSymbol().getSymbol());
+            }
+        }
     }
 
     public void makeMove(Game game) throws InvalidMoveException {
         game.makeMove();
+        logMove(game);
     }
 
     public GameState checkState(Game game) {
@@ -29,5 +37,20 @@ public class GameController {
 
     public void printBoard(Game game) {
         game.printBoard();
+    }
+
+    private void logMove(Game game) {
+        Move lastMove = game.getLastMove();
+        if (lastMove != null) {
+            System.out.println("Player " + lastMove.getPlayer().getName() + " moved to position (" + lastMove.getPosition().getRow() + ", " + lastMove.getPosition().getCol() + ")");
+        }
+    }
+
+    public void printMovesHistory(Game game) {
+        List<Move> moves = game.getMovesHistory();
+        System.out.println("Moves History:");
+        for (Move move : moves) {
+            System.out.println("Player " + move.getPlayer().getName() + " moved to position (" + move.getPosition().getRow() + ", " + move.getPosition().getCol() + ")");
+        }
     }
 }
