1a61c095d8d59b9ea10f436842abf8b385b0a87a
lilli2203 <mailyourlilli@gmail.com>
2024-07-12 11:14:13 +0530

    made changes to WinningAlgorithm.java

--- WinningAlgorithm.java
+++ WinningAlgorithm.java
@@ -6,74 +6,64 @@ import com.example.models.Move;
 import java.util.HashMap;
 
 public class WinningAlgorithm {
-    HashMap<Integer, HashMap<Character, Integer>> rowMaps = new HashMap<>();
-    HashMap<Integer, HashMap<Character, Integer>> colMaps = new HashMap<>();
-    HashMap<Character, Integer> leftDiagonalMap = new HashMap<>();
-    HashMap<Character, Integer> rightDiagonalMap = new HashMap<>();
+    private HashMap<Integer, HashMap<Character, Integer>> rowMaps = new HashMap<>();
+    private HashMap<Integer, HashMap<Character, Integer>> colMaps = new HashMap<>();
+    private HashMap<Character, Integer> leftDiagonalMap = new HashMap<>();
+    private HashMap<Character, Integer> rightDiagonalMap = new HashMap<>();
 
     public boolean checkWinner(Board board, Move move) {
         int row = move.getCell().getRow();
         int col = move.getCell().getCol();
         Character character = move.getPlayer().getSymbol().getaChar();
 
-        //Row
-        if (!rowMaps.containsKey(row)) {
-            rowMaps.put(row, new HashMap<>());
-        }
-        HashMap<Character, Integer> currRowMap = rowMaps.get(row);
-
-        if (!currRowMap.containsKey(character)) {
-            currRowMap.put(character, 1);
-        } else {
-            currRowMap.put(character, currRowMap.get(character) + 1);
-        }
-
-        if (currRowMap.get(character) == board.getSize()) {
+        if (updateAndCheck(rowMaps, row, character, board.getSize())) {
             return true;
         }
 
-        //Col
-        if (!colMaps.containsKey(col)) {
-            colMaps.put(col, new HashMap<>());
-        }
-        HashMap<Character, Integer> currColMap = colMaps.get(col);
-
-        if (!currColMap.containsKey(character)) {
-            currColMap.put(character, 1);
-        } else {
-            currColMap.put(character, currColMap.get(character) + 1);
-        }
-
-        if (currColMap.get(character) == board.getSize()) {
+        if (updateAndCheck(colMaps, col, character, board.getSize())) {
             return true;
         }
 
-        //Left Diagonal
         if (row == col) {
-            if (!leftDiagonalMap.containsKey(character)) {
-                leftDiagonalMap.put(character, 1);
-            } else {
-                leftDiagonalMap.put(character, leftDiagonalMap.get(character) + 1);
-            }
-
-            if (leftDiagonalMap.get(character) == board.getSize()) {
+            if (updateAndCheckDiagonal(leftDiagonalMap, character, board.getSize())) {
                 return true;
             }
         }
 
-        //Right Diagonal
         if (row + col == board.getSize() - 1) {
-            if (!rightDiagonalMap.containsKey(character)) {
-                rightDiagonalMap.put(character, 1);
-            } else {
-                rightDiagonalMap.put(character, rightDiagonalMap.get(character) + 1);
-            }
-
-            if (rightDiagonalMap.get(character) == board.getSize()) {
+            if (updateAndCheckDiagonal(rightDiagonalMap, character, board.getSize())) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    private boolean updateAndCheck(HashMap<Integer, HashMap<Character, Integer>> map, int index, Character character, int boardSize) {
+        map.putIfAbsent(index, new HashMap<>());
+        HashMap<Character, Integer> charCountMap = map.get(index);
+        charCountMap.put(character, charCountMap.getOrDefault(character, 0) + 1);
+
+        return charCountMap.get(character) == boardSize;
+    }
+
+    private boolean updateAndCheckDiagonal(HashMap<Character, Integer> map, Character character, int boardSize) {
+        map.put(character, map.getOrDefault(character, 0) + 1);
+
+        return map.get(character) == boardSize;
+    }
+
+    public void reset() {
+        rowMaps.clear();
+        colMaps.clear();
+        leftDiagonalMap.clear();
+        rightDiagonalMap.clear();
+    }
+
+    public void printState() {
+        System.out.println("Row Maps: " + rowMaps);
+        System.out.println("Column Maps: " + colMaps);
+        System.out.println("Left Diagonal Map: " + leftDiagonalMap);
+        System.out.println("Right Diagonal Map: " + rightDiagonalMap);
     }
 }
