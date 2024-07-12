35514aa1da4e6a15dfa2c3b90afb1b8b883f5694
lilli2203 <mailyourlilli@gmail.com>
2024-07-12 11:18:46 +0530

    made changes

--- PlayerType.java
+++ PlayerType.java
@@ -1,6 +1,21 @@
 package com.example.models;
 
 public enum PlayerType {
-    HUMAN,
-    BOT
+    HUMAN("Human Player"),
+    BOT("Computer Player");
+
+    private String description;
+
+    PlayerType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
 }
