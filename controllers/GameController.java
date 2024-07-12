4fd6b6ddf359aa70839143c205dbc421b279e276
lilli2203 <mailyourlilli@gmail.com>
2024-07-12 11:19:57 +0530

    Update GameController.java

--- controllers/GameController.java
+++ controllers/GameController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 public class GameController {
+    
     public Game startGame(int dimension, List<Player> players) throws InvalidMoveException {
         validatePlayers(players);
         return new Game(dimension, players);
@@ -25,6 +26,7 @@ public class GameController {
     public void makeMove(Game game) throws InvalidMoveException {
         game.makeMove();
         logMove(game);
+        updateGameState(game);
     }
 
     public GameState checkState(Game game) {
@@ -42,7 +44,7 @@ public class GameController {
     private void logMove(Game game) {
         Move lastMove = game.getLastMove();
         if (lastMove != null) {
-            System.out.println("Player " + lastMove.getPlayer().getName() + " moved to position (" + lastMove.getPosition().getRow() + ", " + lastMove.getPosition().getCol() + ")");
+            System.out.println("Player " + lastMove.getPlayer().getName() + " moved to position (" + lastMove.getCell().getRow() + ", " + lastMove.getCell().getCol() + ")");
         }
     }
 
@@ -50,7 +52,34 @@ public class GameController {
         List<Move> moves = game.getMovesHistory();
         System.out.println("Moves History:");
         for (Move move : moves) {
-            System.out.println("Player " + move.getPlayer().getName() + " moved to position (" + move.getPosition().getRow() + ", " + move.getPosition().getCol() + ")");
+            System.out.println("Player " + move.getPlayer().getName() + " moved to position (" + move.getCell().getRow() + ", " + move.getCell().getCol() + ")");
         }
+    }
+
+    private void updateGameState(Game game) {
+        if (game.getGameState() == GameState.ENDED) {
+            System.out.println("Game ended. Winner: " + game.getWinner().getName());
+        } else if (game.getGameState() == GameState.DRAW) {
+            System.out.println("Game ended in a draw.");
+        }
+    }
+
+    public void restartGame(Game game) throws InvalidMoveException {
+        game.reset();
+        System.out.println("Game restarted.");
+    }
+
+    public void endGame(Game game) {
+        game.setGameState(GameState.ENDED);
+        System.out.println("Game ended.");
+    }
+
+    public void saveGame(Game game) {
+        System.out.println("Game state saved.");
+    }
+
+    public Game loadGame() {
+        System.out.println("Game state loaded.");
+        return new Game(3, List.of(new Player("Player 1", new Symbol('X'), PlayerType.HUMAN), new Player("Player 2", new Symbol('O'), PlayerType.HUMAN)));
     }
 }
