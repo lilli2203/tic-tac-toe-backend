53d536e6c502533b847452eae1c86ae4f891f7c7
lilli2203 <mailyourlilli@gmail.com>
2024-07-12 11:09:39 +0530

    made changes to Player.java

--- Player.java
+++ Player.java
@@ -6,7 +6,6 @@ public class Player {
     private String name;
     private Symbol symbol;
     private PlayerType playerType;
-    private static Scanner scanner = new Scanner(System.in);
 
     public Player(String name, Symbol symbol, PlayerType playerType) {
         this.name = name;
@@ -15,13 +14,22 @@ public class Player {
     }
 
     public Move makeMove(Board board) {
-        //Ask the user where they want to place the symbol.
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the row number where you want to make a move ?");
-        int row = scanner.nextInt();
+        int row, col;
+        while (true) {
+            System.out.println(name + ", enter the row number where you want to make a move:");
+            row = scanner.nextInt();
 
-        System.out.println("Please enter the col number where you want to make a move ?");
-        int col = scanner.nextInt();
+            System.out.println(name + ", enter the column number where you want to make a move:");
+            col = scanner.nextInt();
+
+            if (row >= 0 && row < board.getSize() && col >= 0 && col < board.getSize() && board.isValidMove(row, col)) {
+                break;
+            } else {
+                System.out.println("Invalid move. Please enter a valid row and column.");
+            }
+        }
 
         return new Move(new Cell(row, col), this);
     }
